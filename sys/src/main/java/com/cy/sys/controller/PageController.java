6a09354e5cd72bb59a716514448d6c086fcbe60c
Erharichang <baiyun0369@gmail.com>
2020-10-29 11:35:42 +0800

    Initial commit

--- sys/src/main/java/com/cy/sys/controller/PageController.java
+++ sys/src/main/java/com/cy/sys/controller/PageController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * @author: 晓白
  * @time: 2020/10/23 17:45
- * @description:
+ * @description: 将html映射到访问路径
  */
+
 @Controller
 public class PageController {
   @RequestMapping("doIndexUI")
